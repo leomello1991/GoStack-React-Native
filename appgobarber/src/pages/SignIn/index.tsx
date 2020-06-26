ba597ed74515e8493521ce13fc39fcd8b6c5504c
leonardo de Melo <leonardomelo60260@gmail.com>
2020-06-26 17:40:48 -0300

    Fisrt Commit

---
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image } from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logImg from '../../assets/logo.png';
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logImg} />
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Deu');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
+};
+
+export default SignIn;
